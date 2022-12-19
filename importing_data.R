7b80c38a540907f595af3a7427f8cb12c4365cde
Flavio2302 <74097452+Flavio2302@users.noreply.github.com>
2022-12-19 17:48:10 +0100

    importing data

---
+++ importing_data.R
@@ -0,0 +1,3 @@
+train_metadata <- read.csv("/home/fa25225/plasticc/data/plasticc_raw/plasticc_train_metadata.csv.gz")
+train_lightcurves <- read.csv("/home/fa25225/plasticc/data/plasticc_raw/plasticc_train_lightcurves.csv.gz")
+test_metadata <- read.csv("/home/fa25225/plasticc/data/plasticc_raw/plasticc_test_metadata.csv.gz")
