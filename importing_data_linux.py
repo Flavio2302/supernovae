4aaea6976a973eba7a82411eba3fc2aaa59d469b
Flavio2302 <74097452+Flavio2302@users.noreply.github.com>
2022-12-21 09:58:27 +0100

    import python linux

---
+++ importing_data_linux.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#importing objects metadata
+train_metadata = pd.read_csv(r'/home/fa25225/plasticc/data/plasticc_raw/plasticc_train_metadata.csv.gz')
+test_metadata = pd.read_csv(r'/home/fa25225/plasticc/data/plasticc_raw/plasticc_test_metadata.csv.gz')
+
+#importing training set lightcurves
+train_lightcurves = pd.read_csv(r'/home/fa25225/plasticc/data/plasticc_raw/plasticc_train_lightcurves.csv.gz')
+
+# importing test set lightcurves
+indices = ["01","02","03","04","05","06","07","08","09","10","11"]
+df_list = [pd.DataFrame() for i in range(11)]
+
+for i in range(11):
+    df_list[i] = pd.read_csv(r'/home/fa25225/plasticc/data/plasticc_raw/plasticc_test_lightcurves_' + indices[i] + '.csv.gz')
+
+test_lightcurves = pd.concat(df_list)
